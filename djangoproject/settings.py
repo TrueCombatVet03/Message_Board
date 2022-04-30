13a387a39a82a87d8969a8c8a2506d2cb7925df3
Kevin <armyveteranoif247@gmail.com>
2022-04-30 14:00:45 -0500

    update allowed hosts

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-y+yw#816v!_#fffcx@vmi*-lkk5^ym-2i-)baa!%&lv-zi$tv!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '.herokuapp.com'
+]
 
 
 # Application definition
